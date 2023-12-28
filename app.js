51421d7162fa2c35ebf5b72d5ad1c6265134aec5
Nguyen Duc Khanh <khanhnd3@rikkeisoft.com>
2023-12-28 16:56:30 +0900

    Create notification toast

---
+++ app.js
@@ -0,0 +1,51 @@
+var btnWarning = document.querySelector(".control .warning");
+var btnSuccess = document.querySelector(".control .success");
+var btnError = document.querySelector(".control .error");
+
+btnSuccess.addEventListener("click", function () {
+  createToast("success");
+});
+
+btnWarning.addEventListener("click", function () {
+  createToast("warning");
+});
+
+btnError.addEventListener("click", function () {
+  createToast("error");
+});
+
+function createToast(status) {
+  let templateInner = `<i class="fa-solid fa-triangle-exclamation"></i>
+<span class="message">Success notification</span>`;
+  switch (status) {
+    case "success":
+      templateInner = `<i class="fa-regular fa-circle-check"></i>
+
+        <span class="message">Success notification</span>`;
+      break;
+    case "error":
+      templateInner = ` <i class="fa-solid fa-circle-info"></i>
+
+        <span class="message">Error notification</span>`;
+      break;
+    case "warning":
+      templateInner = `<i class="fa-solid fa-triangle-exclamation"></i>
+    <span class="message">waring notification</span>`;
+
+      break;
+  }
+
+  var toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.classList.add(status);
+  toast.innerHTML = `${templateInner} <span class="countdown"></span>`;
+  var toastList = document.getElementById("toasts");
+  toastList.appendChild(toast);
+
+  setTimeout(function () {
+    toastList.style.animation = "slide_hide 2s ease forwards";
+  }, 3000);
+  setTimeout(function () {
+    toast.remove();
+  }, 5000);
+}
